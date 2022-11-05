e2089135365ce577402438cea1d24c8b1b2c3cf9
rakennus <54484559+rakennus@users.noreply.github.com>
2022-11-05 19:00:55 +0100

    Update decoder.sh

--- server/decoder.sh
+++ server/decoder.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# mdb-json ~/Downloads/test/Beispieldatenbank.lup ABP_Fachgruppen > test.json
-
 tables=("ABP_Fachgruppen" "ABP_Faecher" "ABP_Kursarten" "ABP_Lehrer" "ABP_NichtMoeglAbiFachKombi" "ABP_Schueler" "ABP_SchuelerFaecher" "ABP_SchuelerFaecherBasisSicherung" "ABP_SchuelerFaecherSicherung" "ABP_SchuelerFehlermeldungen" "ABP_SchuelerSprachenfolge" "ABP_Schuldaten" "ABP_Version")
 
 echo -e "{" > file.json
@@ -9,10 +7,10 @@ echo -e "{" > file.json
 for i in "${tables[@]}"
 do
     :
-    echo $i
     echo -e  '"'$i'": [' >> file.json
-    mdb-json ~/Downloads/test/Beispieldatenbank.lup $i > tmp.json
+    mdb-json $1 $i > tmp.json
     sed 's/$/,/' tmp.json >> file.json
+    rm tmp.json
     echo -e "]," >> file.json
 done
 
